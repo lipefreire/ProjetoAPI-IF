986d809682c200bcf7a81b7e8d475ec4f4d8fce7
Felipe Freire <felipef12123@gmail.com>
2023-02-02 14:28:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.json({
+        titulo: "Teste de API Rest"
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080...")
+});
